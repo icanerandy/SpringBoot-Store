5a2b302232f00c9903c34554c1324ce1811f21dc
icaner <icanerandy@gmail.com>
2023-06-07 22:30:55 +0800

    调整控制层
    1. 提供控制层基类（BaseController）：响应状态码，统一处理异常的方法
    2. 简化UserController中的用户注册reg()方法

---
+++ src/main/java/com/cy/store/controller/BaseController.java
@@ -0,0 +1,25 @@
+package com.cy.store.controller;
+
+import com.cy.store.service.ex.InsertException;
+import com.cy.store.service.ex.ServiceException;
+import com.cy.store.service.ex.UsernameDuplicateException;
+import com.cy.store.util.JsonResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/** 控制器类的基类 */
+public class BaseController {
+    /** 操作成功的状态码 */
+    public static final int OK = 200;
+
+    /** @ExceptionHandler用于统一处理方法抛出的异常 */
+    @ExceptionHandler(ServiceException.class)
+    public JsonResult<Void> handlerException(Throwable e) {
+        JsonResult<Void> result = new JsonResult<>(e);
+        if (e instanceof UsernameDuplicateException) {
+            result.setState(4000);
+        } else if (e instanceof InsertException) {
+            result.setState(5000);
+        }
+        return result;
+    }
+}
--- src/main/java/com/cy/store/controller/UserController.java
+++ src/main/java/com/cy/store/controller/UserController.java
@@ -2,24 +2,25 @@ package com.cy.store.controller;
 
 import com.cy.store.entity.User;
 import com.cy.store.service.IUserService;
-import com.cy.store.service.ex.InsertException;
-import com.cy.store.service.ex.UsernameDuplicateException;
 import com.cy.store.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/* 处理用户相关请求的控制器类 */
+
+/** 处理用户相关请求的控制器类 */
 @RestController
 @RequestMapping("users")
-public class UserController {
+public class UserController extends BaseController {
     @Autowired
     private IUserService userService;
 
     @RequestMapping("reg")
     public JsonResult<Void> reg(User user) {
         /* 创建返回值 */
-        JsonResult<Void> result = new JsonResult<Void>();
+        JsonResult<Void> result = new JsonResult<>();
+
+/*
         try {
             // 调用业务对象执行注册
             userService.reg(user);
@@ -27,7 +28,7 @@ public class UserController {
             result.setState(200);
         } catch (UsernameDuplicateException e) {
             // 用户名被占用
-            result.setState(400);
+            result.setState(4000);
             result.setMessage("用户名已经被占用");
         } catch (InsertException e) {
             // 插入数据异常
@@ -36,5 +37,12 @@ public class UserController {
         }
 
         return result;
+*/
+
+        // 调用业务对象执行注册
+        userService.reg(user);
+
+        // 返回
+        return new JsonResult<>(OK);
     }
 }
