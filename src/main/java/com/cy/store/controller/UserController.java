d4b0987e3365f3f7b27ad8f7d9b4360c8a8b7637
icaner <icanerandy@gmail.com>
2023-06-07 22:13:11 +0800

    处理请求
    1. 创建UserController控制类
    2. 在控制类中添加处理请求的用户注册方法

--- src/main/java/com/cy/store/controller/UserController.java
+++ src/main/java/com/cy/store/controller/UserController.java
@@ -1,2 +1,40 @@
-package com.cy.store.controller;public class UserController {
+package com.cy.store.controller;
+
+import com.cy.store.entity.User;
+import com.cy.store.service.IUserService;
+import com.cy.store.service.ex.InsertException;
+import com.cy.store.service.ex.UsernameDuplicateException;
+import com.cy.store.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/* 处理用户相关请求的控制器类 */
+@RestController
+@RequestMapping("users")
+public class UserController {
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping("reg")
+    public JsonResult<Void> reg(User user) {
+        /* 创建返回值 */
+        JsonResult<Void> result = new JsonResult<Void>();
+        try {
+            // 调用业务对象执行注册
+            userService.reg(user);
+            // 响应成功
+            result.setState(200);
+        } catch (UsernameDuplicateException e) {
+            // 用户名被占用
+            result.setState(400);
+            result.setMessage("用户名已经被占用");
+        } catch (InsertException e) {
+            // 插入数据异常
+            result.setState(5000);
+            result.setMessage("注册失败，请联系系统管理员");
+        }
+
+        return result;
+    }
 }
